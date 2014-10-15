a8a5f69f08e28e7b6002f6f4310d69aab09ad4c6
Micah Elizabeth Scott <micah@scanlime.org>
2014-10-14 23:39:29 -0700

    Doc updates, ipython polish

--- backdoor/shell.py
+++ backdoor/shell.py
@@ -10,9 +10,9 @@ from IPython.core.magic_arguments import magic_arguments, argument, parse_argstr
 from IPython.terminal.embed import InteractiveShellEmbed
 
 from remote import Device
+from hilbert import hilbert
 from dump import *
 from code import *
-from hilbert import *
 
 d = Device()
 
@@ -25,60 +25,59 @@ class ShellMagics(Magics):
     @line_magic
     @magic_arguments()
     @argument('address', type=hexint)
-    @argument('size', type=hexint, nargs='?', default=0x100)
+    @argument('size', type=hexint, nargs='?', default=0x100,
+        help='Address to read from. Hexadecimal. Not necessarily aligned.')
     def rd(self, line):
-        """Read ARM memory block
-
-        Usage:
-          %rd address [size]
-
-        Output is hex dumped and saved as "result.log" on disk.
-        All parameters in hexadecimal.
-        """
-
+        """Read ARM memory block"""
         args = parse_argstring(self.rd, line)
         dump(d, args.address, args.size)
 
     @line_magic
     @magic_arguments()
-    @argument('address', type=hexint)
-    @argument('word', type=hexint)
+    @argument('address', type=hexint, help='Hex address')
+    @argument('word', type=hexint, help='Hex word')
     def po(self, line):
-        """Poke one word into ARM memory
-
-        Usage:
-          %po address word
-        """
+        """Poke one word into ARM memory"""
         args = parse_argstring(self.po, line)
         d.poke(args.address, args.word)
 
     @line_magic
     @magic_arguments()
-    @argument('address', type=hexint)
-    @argument('word', type=hexint)
-    @argument('count', type=hexint)
+    @argument('address', type=hexint, help='Hex address, word aligned')
+    @argument('word', type=hexint, help='Hex word')
+    @argument('count', type=hexint, help='Hex wordcount')
     def fill(self, line):
-        """Fill contiguous words in ARM memory with the same value
-
-        Usage:
-          %fill address word count
-        """
+        """Fill contiguous words in ARM memory with the same value"""
         args = parse_argstring(self.fill, line)
         for i in range(args.count):
             d.poke(args.address + i*4, args.word)
 
     @line_magic
     @magic_arguments()
-    @argument('address', type=hexint)
-    @argument('size', type=hexint, nargs='?', default=0x100)
+    @argument('address', type=hexint, help='Hex address')
+    @argument('size', type=hexint, nargs='?', default=0x100,
+        help='Hex byte count')
     def dis(self, line):
-        """Disassemble ARM instructions
-
-        Usage:
-          %dis address [size]
-        """
+        """Disassemble ARM instructions"""
         args = parse_argstring(self.dis, line)
         print disassemble(d, args.address, args.size)
+
+    @line_cell_magic
+    @magic_arguments()
+    @argument('-b', '--base', type=int, default=0,
+        help='First address in map')
+    @argument('-s', '--scale', type=int, default=256,
+        help='Scale in bytes per pixel')
+    @argument('-w', '--width', type=int, default=4096,
+        help='Size of square hilbert map, in pixels')
+    @argument('x', type=int)
+    @argument('y', type=int)
+    def msl(self, line, cell=''):
+        """Memsquare lookup"""
+        args = parse_argstring(self.msl, line)
+        print args.base, args.scale, args.width
+        addr = args.base + args.scale * hilbert(args.x, args.y, args.width) 
+        print "%08x" % addr
 
     @line_cell_magic
     @magic_arguments()
@@ -90,13 +89,13 @@ class ShellMagics(Magics):
         NOTE that the assembled instructions will be padded to the
         nearest 32-bit word.
 
-        Usage:
-          %asm address code
+        Use with line or cell mode:
 
-        Or in cell mode:
-          %%asm address
-          code...
-          code...
+            %asm address line
+
+            %%asm address
+            line..
+            line..
 
         """
         args = parse_argstring(self.asm, line)
