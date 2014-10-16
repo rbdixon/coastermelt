6dd532d75717481375c0b4992ebbd0914d0a6d72
Micah Elizabeth Scott <micah@misc.name>
2014-10-16 08:22:48 -0700

    More useful description for trap

--- backdoor/trap.py
+++ backdoor/trap.py
@@ -13,38 +13,9 @@ import remote, sys
 # in flash. If the mapping is longer, the region repeats. The mapping can go
 # anywhere in the low 8MB of virtual address space.
 #
-# Right now, here's what you get:
-#    
-#    In []: trap 7f_ 100
-#    
-#    In []: dis 7f_
-#    007f0000    pop {r3, r4, r5, r6, r7, pc}
-#    007f0002    push    {r0, r1, r4, r5, r6, r7, lr}
-#    007f0004    movs    r7, r0
-#    007f0006    movs    r0, #192    ; 0xc0
-#    007f0008    sub sp, #4
-#    007f000a    blx 0x007206d8
-#    007f000e    ldr r2, [pc, #964]  ; (0x007f03d4)
-#    007f0010    ldr r1, [r2, #4]
-#    007f0012    asrs    r3, r2, #18
-#    007f0014    orrs    r1, r3
-#    007f0016    str r1, [r2, #4]
-#    007f0018    ldr r1, [r2, #4]
-#    007f001a    asrs    r3, r2, #17
-#    007f001c    orrs    r1, r3
-#
-# So, in short, right now the "trap" implementation replaces any instruction
-# you like with an instruction that copies:
-#
-#    r3 = [sp + 0]
-#    r4 = [sp + 4]
-#    r5 = [sp + 8]
-#    r6 = [sp + c]
-#    r7 = [sp + 10]
-#    pc = [sp + 14]
-#
-# How about that sp+14?
-#
+# With the minimum length, you get a pop {r3-r7, pc} instruction you can put
+# anywhere in flash. You can use it to crash some functions. Others, it will
+# return early.
 
 __all__ = [
     'trap_set',
