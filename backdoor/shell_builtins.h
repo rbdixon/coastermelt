068decbe3a1f0a51158f3c9b01255d5c1c060093
Micah Elizabeth Scott <micah@misc.name>
2014-10-19 17:44:46 -0700

    Smaller types for the shell

--- backdoor/shell_builtins.h
+++ backdoor/shell_builtins.h
@@ -1,7 +1,11 @@
 /*
- * This C++ header file is available by default in code compiled
- * for the coastermelt backdoor. Put useful hardware definitions
- * and shortcuts here!
+ * This C++ header file is available by default in code compiled for the
+ * coastermelt backdoor. Put useful hardware definitions and shortcuts here!
+ *
+ * Things in this file should generally only be used interactively. It's bad
+ * form to write code that depends on these definitions, there's usually a
+ * slightly longer way to do the same thing that's more maintainable and more
+ * readable.
  */
 
 #pragma once
@@ -17,3 +21,13 @@ using namespace MT1939;
 static uint32_t *wordp = (uint32_t *) pad;
 static uint16_t *halfp = (uint16_t *) pad;
 static uint8_t  *bytep = (uint8_t *) pad;
+
+// Shorter type names for interactive use
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int8_t   i8;
+typedef int16_t  i16;
+typedef int32_t  i32;
+typedef int64_t  i64;
