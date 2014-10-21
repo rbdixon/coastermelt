533742334636a5fbb1975f812d98b7b6df51ec64
Micah Elizabeth Scott <micah@misc.name>
2014-10-20 23:30:49 -0700

    Half-baked experiment for eject IPC

--- backdoor/shell_builtins.h
+++ backdoor/shell_builtins.h
@@ -41,20 +41,46 @@ typedef volatile int32_t  vi32;
 typedef volatile int64_t  vi64;
 
 
-//// Half-baked things that don't have a good home yet
+// Half-baked things that don't have a good home yet
 
-void ipc_send_4206000(unsigned op0, unsigned op1)
+
+void ipc_eject()
 {
-	// Send some kind of inter-processor message. ARM sends (0x22b, 0x7fff) to
-	// eject the disc, and another processor taps the solenoid 4 times in a loop.
-	// Sending this command on its own, however, doesn't produce the eject.
+	/*
+	 * Setup:
+	 * 
+	 *    1. Push in tray completely, no disc
+	 *    2. Eject completely
+	 *    3. Press in slowly, until light begins blinking but before mechanical latch
+	 *    4. Tape tray at this position
+	 *    5. Verify that %eject can be repeatedly issued.
+	 *       Each time, you should hear 4 audible clicks from the eject solenoid.
+	 *       Taping the tray just right will make this click much louder.
+	 *
+	 * After this, "%ec ipc_eject()" will call this function and cause 1
+	 * click. This function on its own isn't enough to set up the eject or to
+	 * actually make the tray eject when it's all the way in, but it's a
+	 * start.
+	 *
+	 * I'm calling this "IPC" because it seems extremely likely this is a
+	 * command to another CPU rather than a direct hardware operation, though
+	 * now that I've produced 1 click rather than 4 I'm not so sure.
+	 */
+
+	auto& reg_frob1 = *(volatile uint32_t*) 0x4002088;
+	auto& reg_frob2 = *(volatile uint32_t*) 0x4002010;
+
+	reg_frob1 |= 0x20000;
+	reg_frob1 |= 2;
+	reg_frob1 &= ~0x200;
+	reg_frob2 = (reg_frob2 & ~3) | 0xff00;
 
 	auto& reg_op1    = *(volatile uint32_t*) 0x4206000;
 	auto& reg_status = *(volatile uint32_t*) 0x4206004;
 	auto& reg_op0    = *(volatile uint32_t*) 0x4206010;
 
-	reg_op0 = op0;
-	reg_op1 = op1;
+	reg_op0 = 0x22b;
+	reg_op1 = 0x7fff;
 
 	while (reg_status & 0x80000000);
 }
