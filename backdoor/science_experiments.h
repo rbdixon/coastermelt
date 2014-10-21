8fcb6be098d7efa86c01d97c9231481eb2deb5fd
Micah Elizabeth Scott <micah@misc.name>
2014-10-21 00:39:22 -0700

    Can rattle the solenoid sometimes

--- backdoor/science_experiments.h
+++ backdoor/science_experiments.h
@@ -1,51 +1,91 @@
 // Half baked, works in progress...
 
 #pragma once
+#include <stdint.h>
+#include "ts01_defs.h"
 
 
-int ipc_eject()
+int ipc_send_4206000(unsigned op0, unsigned op1)
 {
-	/*
-	 * Setup:
-	 * 
-	 *    1. Push in tray completely, no disc
-	 *    2. Eject completely
-	 *    3. Press in slowly, until light begins blinking but before mechanical latch
-	 *    4. Tape tray at this position
-	 *    5. Verify that %eject can be repeatedly issued.
-	 *       Each time, you should hear 4 audible clicks from the eject solenoid.
-	 *       Taping the tray just right will make this click much louder.
-	 *
-	 * After this, "%ec ipc_eject()" will call this function and cause 1
-	 * click. This function on its own isn't enough to set up the eject or to
-	 * actually make the tray eject when it's all the way in, but it's a
-	 * start.
-	 *
-	 * I'm calling this "IPC" because it seems extremely likely this is a
-	 * command to another CPU rather than a direct hardware operation, though
-	 * now that I've produced 1 click rather than 4 I'm not so sure.
-	 */
-
-	auto& reg_frob1 = *(volatile uint32_t*) 0x4002088;
-	auto& reg_frob2 = *(volatile uint32_t*) 0x4002010;
-
-	reg_frob1 |= 0x20000;
-	reg_frob1 |= 2;
-	reg_frob1 &= ~0x200;
-	reg_frob2 = (reg_frob2 & ~3) | 0xff00;
-
 	auto& reg_op1    = *(volatile uint32_t*) 0x4206000;
 	auto& reg_status = *(volatile uint32_t*) 0x4206004;
 	auto& reg_op0    = *(volatile uint32_t*) 0x4206010;
 
-	reg_op0 = 0x22b;
-	reg_op1 = 0x7fff;
+	reg_op0 = op0;
+	reg_op1 = op1;
 
 	int loops = 0;
-	while (reg_status & 0x80000000)
+	while (reg_status & 0x80000000);
 		loops++;
-
-	SysTime::wait_ms(500);
-
 	return loops;
 }
+
+
+int ipc_eject_4200000()
+{
+	// Thought this IPC-looking port was part of Eject, doesn't seem to
+	// be necessary for the solenoid but it seems important for control
+	// flow in timing the eject command, or maybe blocking SCSI traffic
+	// during the operation.
+
+	return ipc_send_4206000(0x22b, 0x7fff);
+}
+
+
+
+auto& reg_tray_mmio = *(volatile uint32_t*) 0x4002088;
+
+void set_led(bool on)
+{
+	if (on)
+		reg_tray_mmio &= ~0x2000000;
+	else
+		reg_tray_mmio |= 0x2000000;
+}
+
+void set_solenoid(bool on)
+{
+	if (on) {
+		reg_tray_mmio |= 0x20000;
+		reg_tray_mmio |= 2;
+		reg_tray_mmio &= ~0x200;
+	} else {
+		reg_tray_mmio &= ~2;
+		reg_tray_mmio |= 0x200;
+	}
+}
+
+void blink_led(unsigned count = 4)
+{
+	// Seems to work at any time during normal firmware
+	unsigned saved = begin_critical_section();
+	while (count--) {
+		set_led(1);
+		SysTime::wait_ms(100);
+		set_led(0);
+		SysTime::wait_ms(250);
+	}
+	end_critical_section(saved);
+}
+
+void buzz_solenoid()
+{
+	// Prereqs:
+	//   - Drive opened
+	//   - Drive closed half-way and taped
+	//   - Eject run, tray doesn't actually move, 4 clicks audible
+	//
+	// Now when this runs, the solenoid rattles.
+
+	unsigned count = 50;
+	unsigned saved = begin_critical_section();
+	while (count--) {
+		set_solenoid(1);
+		SysTime::wait_ms(4);
+		set_solenoid(0);
+		SysTime::wait_ms(4);
+	}
+	end_critical_section(saved);
+}
+
+
